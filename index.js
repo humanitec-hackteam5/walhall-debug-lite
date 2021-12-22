e21d920a13fa727da4729f9bd67cbebb5eccc3ce
Nikolay Kovalenko <nikolay.kovalenko@humanitec.com>
2021-12-22 13:03:57 +0100

    second commit

--- index.js
+++ index.js
@@ -1,41 +1,55 @@
 const http = require('http');
-const pg = require('pg');
+
+async function testDatabase(user, password, database, host, port) {
+  const { Client } = require('pg')
+  const client = new Client({
+    user: user,
+    password: password,
+    database: database,
+    host: host,
+    port: port
+  })
+
+  const dbReport = {}
+  await client.connect(err => {
+    if (err) {
+      dbReport.error = err.toString()
+    }
+  })
+  if (!dbReport.error) {
+    await client.query('SELECT NOW()', (err, res) => {
+        if (err) {
+          dbReport.error = err.toString()
+        } else {
+          dbReport.result = res
+        }
+      })    
+    await client.end()
+  }
+
+  return dbReport
+}
 
 const httpServer = http.createServer((req, res) => {
   // Log all requests made to the server
-  console.log(`${req.method} ${req.url}`);
+  console.log(`${req.method} ${req.url}`)
 
-  const client = new pg.Client({
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
-    host: process.env.DATABASE_HOST,
-    port: process.env.DATABASE_PORT
-  });
-
-  function success() {
-    return true;
-  }
-
-  function failure(err) {
-    console.log(err.toString());
-    return false
-  }
-
-  connected = client.connect().then(success, failure);
-  queried = client.query("SELECT 1").then(success, failure);
-  client.end().then(success, failure);
-
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  const data = {
-    env : process.env,
-    database: {
-      connected : connected,
-      queried: queried
+  testDatabase(
+    process.env.DATABASE_USER,
+    process.env.DATABASE_PASSWORD,
+    process.env.DATABASE_NAME,
+    process.env.DATABASE_HOST,
+    process.env.DATABASE_PORT
+  )
+  .then(async (dbReport) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json')
+    const data = {
+      env : process.env,
+      database: dbReport
     }
-  }
-  res.end(JSON.stringify(data));
-});
+    res.end(JSON.stringify(data))  
+  })
+})
 
-httpServer.listen(process.env.PORT || '8080');
+httpServer.listen(process.env.PORT || '8080')
